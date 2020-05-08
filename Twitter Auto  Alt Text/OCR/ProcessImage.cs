1687a4729f061c7ba8962fdca2c69736a4b299aa
Neptune <eugeneogongo@live.com>
2020-05-09 00:20:56 +0300

    Feature: backend is ready

--- Twitter Auto  Alt Text/OCR/ProcessImage.cs
+++ Twitter Auto  Alt Text/OCR/ProcessImage.cs
@@ -1,8 +1,12 @@
 ﻿using FileUploadService.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Tesseract;
 
@@ -10,15 +14,17 @@ namespace Twitter_Auto__Alt_Text.OCR
 {
     public class ProcessImage
     {
-       private static TesseractEngine tesseractEngine;
+        private static TesseractEngine tesseractEngine;
         public static string ExtractText(string path)
         {
             try
             {
-                var result = tesseractEngine.Process(Pix.LoadFromFile(path));
+                tesseractEngine = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default);
+
+                var result = tesseractEngine.Process(Pix.LoadFromFile(path), PageSegMode.Auto);
                 return result.GetText().Trim();
 
-            }catch(Exception ex)
+            } catch (Exception ex)
             {
                 Log.E(ex.Message);
                 return null;
@@ -34,6 +40,23 @@ namespace Twitter_Auto__Alt_Text.OCR
             {
                 tesseractEngine = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default);
             }
+        }
+        public static string GetGrayedImage(string path)
+        {
+            var filepath = Path.GetPathRoot(path);
+            var filename = Path.GetFileName(path);
+            return Path.Combine(filepath, "g" + filename);
+        }
+
+
+    public static void DeleteFile(string filename)
+        {
+            Thread t = new Thread(() =>
+            {
+                File.Delete(filename);
+            });
+
+            t.Start();
         }
     }
 }
